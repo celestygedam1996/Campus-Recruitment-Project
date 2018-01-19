8bbdb4f85748844c49b7a7a73a8813d0ff61660c
spiyush <gitpiyushmoon@gmail.com>
2018-01-19 11:20:56 +0530

    database
    database

--- campus.sql
+++ campus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2017 at 11:55 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: Jan 19, 2018 at 06:50 AM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,12 +31,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `batch` (
   `batch_id` int(200) NOT NULL,
   `batch_name` varchar(200) NOT NULL,
-  `batch_year` int(20) NOT NULL,
-  `batch_branch` varchar(200) NOT NULL,
-  `batch_skills` varchar(300) NOT NULL,
+  `batch_code` int(20) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `batch_desc` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `batch`
+--
+
+INSERT INTO `batch` (`batch_id`, `batch_name`, `batch_code`, `created_at`, `updated_at`, `batch_desc`) VALUES
+(4, 'cse', 54, '2018-01-13 10:09:42', '0000-00-00 00:00:00', 'asdasadad');
 
 -- --------------------------------------------------------
 
@@ -45,10 +51,10 @@ CREATE TABLE `batch` (
 --
 
 CREATE TABLE `hr` (
-  `hr_id` int(200) NOT NULL,
-  `hr_name` varchar(200) NOT NULL,
+  `id` int(200) NOT NULL,
+  `username` varchar(200) NOT NULL,
   `hr_email` varchar(200) NOT NULL,
-  `hr_password` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `hr_company` varchar(200) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
@@ -58,8 +64,9 @@ CREATE TABLE `hr` (
 -- Dumping data for table `hr`
 --
 
-INSERT INTO `hr` (`hr_id`, `hr_name`, `hr_email`, `hr_password`, `hr_company`, `created_at`, `updated_at`) VALUES
-(1, 'piyushmoon', 'gvsdadh@gmail.com', '$2y$10$SODT.gPuKO12DLh7QEsBseA.0Gosy3ehYbCa1iw1Ku2CYjddXT/ei', 'abcd', '2017-12-09 10:29:49', '0000-00-00 00:00:00');
+INSERT INTO `hr` (`id`, `username`, `hr_email`, `password`, `hr_company`, `created_at`, `updated_at`) VALUES
+(1, 'piyushmoon', 'gvsdadh@gmail.com', '$2y$10$SODT.gPuKO12DLh7QEsBseA.0Gosy3ehYbCa1iw1Ku2CYjddXT/ei', 'abcd', '2017-12-09 10:29:49', '0000-00-00 00:00:00'),
+(7, 'localhostlocalhost', '', '$2y$10$9jgL.B2UsSppCk/nuEUkN.uyYOkWjKPfJo/3OAGJX1Sk52SreMa.O', '', '2018-01-19 03:56:45', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -68,10 +75,10 @@ INSERT INTO `hr` (`hr_id`, `hr_name`, `hr_email`, `hr_password`, `hr_company`, `
 --
 
 CREATE TABLE `tandp` (
-  `tandp_id` int(200) NOT NULL,
-  `tandp_name` varchar(200) NOT NULL,
+  `id` int(200) NOT NULL,
+  `username` varchar(200) NOT NULL,
   `tandp_email` varchar(200) NOT NULL,
-  `tandp_password` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `tandp_college` varchar(200) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
@@ -81,8 +88,9 @@ CREATE TABLE `tandp` (
 -- Dumping data for table `tandp`
 --
 
-INSERT INTO `tandp` (`tandp_id`, `tandp_name`, `tandp_email`, `tandp_password`, `tandp_college`, `created_at`, `updated_at`) VALUES
-(1, 'piyushmoon', 'abcd@gmail.com', '$2y$10$SODT.gPuKO12DLh7QEsBseA.0Gosy3ehYbCa1iw1Ku2CYjddXT/ei', 'rcert', '2017-12-09 10:39:12', '0000-00-00 00:00:00');
+INSERT INTO `tandp` (`id`, `username`, `tandp_email`, `password`, `tandp_college`, `created_at`, `updated_at`) VALUES
+(1, 'piyushmoon', 'abcd@gmail.com', '$2y$10$SODT.gPuKO12DLh7QEsBseA.0Gosy3ehYbCa1iw1Ku2CYjddXT/ei', 'rcert', '2017-12-09 10:39:12', '0000-00-00 00:00:00'),
+(3, 'localhostlocalhost', '', '$2y$10$T91q1nkmTS4E1Jon/NgRy.EKQvjuV2LapKbjZir6cezccGTvBgbyS', '', '2018-01-19 04:19:14', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -100,6 +108,15 @@ CREATE TABLE `vacancy` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `vacancy`
+--
+
+INSERT INTO `vacancy` (`vacancy_id`, `vacancy_name`, `vacancy_desc`, `vacancy_type`, `created_at`, `upadted_at`) VALUES
+(3, 'dasdasd', 'adsadsa', 1, '2017-12-23 10:12:31', '0000-00-00 00:00:00'),
+(5, 'namdfdsf', 'erwrwr', 1, '2018-01-13 10:08:54', '0000-00-00 00:00:00'),
+(6, 'asda', 'asdda', 2, '2018-01-13 10:09:06', '0000-00-00 00:00:00');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -113,13 +130,13 @@ ALTER TABLE `batch`
 -- Indexes for table `hr`
 --
 ALTER TABLE `hr`
-  ADD PRIMARY KEY (`hr_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tandp`
 --
 ALTER TABLE `tandp`
-  ADD PRIMARY KEY (`tandp_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `vacancy`
@@ -135,26 +152,22 @@ ALTER TABLE `vacancy`
 -- AUTO_INCREMENT for table `batch`
 --
 ALTER TABLE `batch`
-  MODIFY `batch_id` int(200) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `batch_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `hr`
 --
 ALTER TABLE `hr`
-  MODIFY `hr_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `tandp`
 --
 ALTER TABLE `tandp`
-  MODIFY `tandp_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `vacancy`
 --
 ALTER TABLE `vacancy`
-  MODIFY `vacancy_id` int(200) NOT NULL AUTO_INCREMENT;
-COMMIT;
+  MODIFY `vacancy_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
